5b94aa864132f6dc2a8b692b02e6ef088e1ab4ca
hasbi abdillah afif <hasbigamer@gmail.com>
2020-01-22 14:01:07 +0700

    init commit

--- aplikasi/lib/aplikasi.dart
+++ aplikasi/lib/aplikasi.dart
@@ -2,4 +2,4 @@ var name = 'Voyager I';
 var year =1977;
 var antennaDiameter = 3.7;
 var FlybyObjects = ['jupiter','saturn']
-var image = {'tags':[saturn],'url':'//path/to/saturn.jpg'};
+var image = {'tags':["saturn"],'url':'//path/to/saturn.jpg'};
---
+++ aplikasi/lib/hari1.dart
@@ -0,0 +1,4 @@
+if (year>=2001){print ('21st century');} else if (year>=1901){print('20th century');} 
+for (var object in flybyobjects) {print (object);}
+for (int month = 1; month <=12;month++ print (month) ;
+while (year <2016) {year +=1}
